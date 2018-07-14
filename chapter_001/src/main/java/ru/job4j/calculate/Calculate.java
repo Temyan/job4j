e058b2beef1940cb4a55909c8ee33d5dbb409228
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-07-14 19:40:55 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,12 @@
 package ru.job4j.calculate;
+/** 
+Выводит сообщение строку на экран
+*/
 public class Calculate {
-    
+    /**
+    @param args - args.
+    @return string on the screen.
+    */
     public static void main (String[] args) {
         System.out.println("Hellow World");
     }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/** 
+*Package for calculate task 
+*
+*@author Artem Maiorov (mailto:maartel@yandex.ru)
+*@version $id$
+*@since 0.1 
+*/
+
+package ru.job4j.calculate;
