73811a1dbd7ba3848eb5e091c16cab677bde3413
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-07-16 18:40:51 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,13 +1,13 @@
 package ru.job4j.calculate;
 /** 
-Выводит сообщение строку на экран
+Выводит сообщение строку на экран.
 */
 public class Calculate {
     /**
     @param args - args.
     @return string on the screen.
     */
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hellow World");
     }
     
