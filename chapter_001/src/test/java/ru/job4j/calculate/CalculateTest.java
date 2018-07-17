3ef37302bbf6f287bfe7fa17a548b626ebd821f2
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-07-17 18:30:13 +0300

    2.3. Запуск программы, запуск тестов, дебаг. [#31169]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,22 +4,22 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
-*Test
-*
-*@author Artem M (mailto:maartel@inbox.ru)
-*@version $id$
-*@since 0.1
-*/
+ *Test
+ *
+ *@author Artem M (mailto:maartel@inbox.ru)
+ *@version $id$
+ *@since 0.1
+ */
 public class CalculateTest {
     /**
-    *Test echo.
-    */ @Test
+     *Test echo.
+     */ @Test
     public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Artem Maiorov";
-    String expect = "Echo, echo, echo : Artem Maiorov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-  }
-    
+        String input = "Artem Maiorov";
+        String expect = "Echo, echo, echo : Artem Maiorov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
