f61640c0382d95ce4dea1114c515856f72ed70af
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-07-22 20:16:54 +0300

    3.2 Конвертер валют

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -9,6 +9,17 @@ public class Converter  {
     public int rubleToEuro(int value) {
         return value / 70;
     }
+
+    /**
+     * Конвентируем евро в рубли.
+     * @param value рубли.
+     * @return евро.
+     *
+     */
+    public int euroToRuble(int value) {
+        return value * 70;
+    }
+
     /**
      * Конвентируем рубли в доллары.
      * @param value рубли
@@ -16,5 +27,14 @@ public class Converter  {
      */
     public int rubleToDollar(int value) {
         return value / 60;
+    }
+
+    /**
+     * Конвентируем доллары в рубли.
+     * @param value рубли
+     * @return Доллары
+     */
+    public int dollarToRuble(int value) {
+        return value * 60;
     }
 }
