bbc530f6bfb818babc5913fc7d29b07c5bc27c44
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-07-24 18:53:47 +0300

    4.2 Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.max;
+
+public class Max {
+    public int max(int first, int second);
+    //return first + second;
+    int resultMax , k;
+    k = 1;
+    resultMax = k > 0 ? int first + int second : 0;
+  return resultMax;
+
+}
