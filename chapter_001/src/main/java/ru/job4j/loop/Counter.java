772623f91312b147b55462b58b002557328ed0de
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-08-02 10:18:43 +0300

    5.1 Подсчет суммы четных чисел

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,15 +1,15 @@
 package ru.job4j.loop;
 
 public class Counter {
+
     public int add(int start, int finish) {
         start = 1;
         finish = 10;
-        while(start <= finish) {
+        for (start < finish; start++) {
             int result = start % 2;
-            if(result == 0) {
+            if (result == 0) {
                 return result += result;
             }
-        }
-        return start;
+                return result;
     }
 }
