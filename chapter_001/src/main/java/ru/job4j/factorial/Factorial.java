9752b59571914ab845577dd20ecba3835e5267c2
mailto:maartel@yandex.ru <mailto:maartel@yandex.ru>
2018-08-06 20:40:29 +0300

    5.1 Подсчет суммы четных чисел

---
+++ chapter_001/src/main/java/ru/job4j/factorial/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.factorial;
+
+public class Factorial {
+    public int calc(int n) {
+        int result = 1;
+            for (int j = 1;  j <= n; j++) {
+                result = result * j;
+            }
+            return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/factorrial/FactorialTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.factorrial;
+
+import ru.job4j.factorial.Factorial;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+
+        Factorial factor = new Factorial();
+        int result = factor.calc(5);
+        assertThat(result, is(120));
+    }
+}
